1c883369a40d25a4c18ec77281857f71a9b6d643
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-07-04 14:59:57 +0000

    Calculator

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,30 +1,11 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import welcome from './brain-games';
-import name from '..';
+import { brainGames, getRandomIntInclusive } from './brain-games';
+import { cons } from '@hexlet/pairs';
 
-welcome();
-console.log('Answer \x1B[31m"yes" \x1B[39mif number even otherwise answer \x1B[31m"no"\x1B[39m.\n\n');
-const userName = name();
-
-const getRandomIntInclusive = (min, max) => {
-  const ceilMin = Math.ceil(min);
-  const floorMax = Math.floor(max);
-  return Math.floor(Math.random() * (floorMax - ceilMin + 1)) + ceilMin;
-};
-
-for (let i = 0; i < 3; i += 1) {
-  const number = getRandomIntInclusive(1, 20);
-  const trueAnswer = number % 2 === 0 ? 'yes' : 'no';
-  const userAnswer = readlineSync.question(`Question: ${number}\nYour answer: `);
-  if (trueAnswer === userAnswer) {
-    console.log('Correct!');
-    if (i === 2) {
-      console.log(`Congratulations, ${userName}!`);
-    }
-  } else {
-    console.log(`\x1B[31m${userAnswer} \x1B[39mis wrong answer ;(. Correct answer was \x1B[31m${trueAnswer}\x1B[39m.`);
-    console.log(`Let's try again, ${userName}!\n`);
-    break;
-  }
+const task = 'Answer \x1B[31m"yes" \x1B[39mif number even otherwise answer \x1B[31m"no"\x1B[39m.\n\n';
+const questionAnswer = () => {
+  const question = getRandomIntInclusive(1, 20);
+  const answer = question % 2 === 0 ? 'yes' : 'no';
+  return cons(question, answer);
 }
+brainGames(task, questionAnswer);
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import name from '..';
+import { car, cdr } from '@hexlet/pairs';
 
-export default () => {
+export const getRandomIntInclusive = (min, max) => {
+  const ceilMin = Math.ceil(min);
+  const floorMax = Math.floor(max);
+  return Math.floor(Math.random() * (floorMax - ceilMin + 1)) + ceilMin;
+};
+
+export const brainGames = (task, questionAnswer) => {
   console.log('\nWelcome to the Brain Games!');
+  console.log(task);
+  const userName = name();
+  for (let i = 0; i < 3; i += 1) {
+    const pairQuestionAnswer = questionAnswer();
+    const question = car(pairQuestionAnswer);
+    const trueAnswer = cdr(pairQuestionAnswer);
+    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+    if (trueAnswer === userAnswer) {
+      console.log('Correct!');
+      if (i === 2) {
+        console.log(`Congratulations, ${userName}!`);
+      }
+    } else {
+      console.log(`\x1B[31m${userAnswer} \x1B[39mis wrong answer ;(. Correct answer was \x1B[31m${trueAnswer}\x1B[39m.`);
+      console.log(`Let's try again, ${userName}!\n`);
+      break;
+    }
+  }
 };
