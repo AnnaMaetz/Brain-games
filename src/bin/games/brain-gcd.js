6fcb3ee60e9d6b72ec4bc76d3dd820e18243ec76
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-07-05 10:38:06 +0000

    test brain-gcd

--- src/bin/games/brain-gcd.js
+++ src/bin/games/brain-gcd.js
@@ -5,40 +5,18 @@ import { brainGames, getRandomIntInclusive } from '../brain-games';
 const task = 'Find the greatest common divisor of given numbers.\n';
 
 
-const gcd = (n1, n2, result, divisor) => {
-  const n2Devisor = (nu1, nu2, r, d) => {
-    const newN2 = nu2 / d;
-    const newResult = r * d;
-    if (nu1 === 1) {
-      return newResult;
-    }
-    return gcd(nu1, newN2, newResult, 2);
-  };
-  const n1Devisor = (num1, num2, res, div) => {
-    const newN1 = num1 / div;
-    if (num2 % div === 0) {
-      return n2Devisor(newN1, num2, res, div);
-    }
-    if (newN1 === 1) {
-      return res;
-    }
-    return gcd(newN1, num2, res, 2);
-  };
-
-  if (n1 < 2) {
-    return result;
+const gcd = (n1, n2) => {
+  if (!n2) {
+    return n1;
   }
-  if (n1 % divisor === 0) {
-    return n1Devisor(n1, n2, result, divisor);
-  }
-  return gcd(n1, n2, result, divisor + 1);
+  return gcd(n2, n1 % n2);
 };
 
 const questionAnswer = () => {
   const number1 = getRandomIntInclusive(1, 30);
   const number2 = getRandomIntInclusive(1, 30);
   const question = `${number1} ${number2}`;
-  const answer = gcd(number1, number2, 1, 2);
+  const answer = gcd(number1, number2);
   return cons(question, String(answer));
 };
 brainGames(task, questionAnswer);
