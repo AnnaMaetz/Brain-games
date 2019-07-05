60478f62458d1059f0e3bb92639d3b8d8d617305
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-07-05 10:15:18 +0000

    test brain-gcd

--- src/bin/games/brain-gcd.js
+++ src/bin/games/brain-gcd.js
@@ -3,33 +3,41 @@ import { cons } from '@hexlet/pairs';
 import { brainGames, getRandomIntInclusive } from '../brain-games';
 
 const task = 'Find the greatest common divisor of given numbers.\n';
+
+
+const gcd = (n1, n2, result, divisor) => {
+  const n2Devisor = (nu1, nu2, r, d) => {
+    const newN2 = nu2 / d;
+    const newResult = r * d;
+    if (nu1 === 1) {
+      return newResult;
+    }
+    return gcd(nu1, newN2, newResult, 2);
+  };
+  const n1Devisor = (num1, num2, res, div) => {
+    const newN1 = num1 / div;
+    if (num2 % div === 0) {
+      return n2Devisor(newN1, num2, res, div);
+    }
+    if (newN1 === 1) {
+      return res;
+    }
+    return gcd(newN1, num2, res, 2);
+  };
+
+  if (n1 < 2) {
+    return result;
+  }
+  if (n1 % divisor === 0) {
+    return n1Devisor(n1, n2, result, divisor);
+  }
+  return gcd(n1, n2, result, divisor + 1);
+};
+
 const questionAnswer = () => {
   const number1 = getRandomIntInclusive(1, 30);
   const number2 = getRandomIntInclusive(1, 30);
   const question = `${number1} ${number2}`;
-
-  const gcd = (n1, n2, result, divisor) => {
-    if (n1 < 2) {
-      return result;
-    }
-    if (n1 % divisor === 0) {
-      const newN1 = n1 / divisor;
-      if (n2 % divisor === 0) {
-        const newN2 = n2 / divisor;
-        const newResult = result * divisor;
-        if (n1 === divisor) {
-          return newResult;
-        }
-        return gcd(newN1, newN2, newResult, 2);
-      }
-      if (n1 === divisor) {
-        return result;
-      }
-      return gcd(newN1, n2, result, 2);
-    }
-
-    return gcd(n1, n2, result, divisor + 1);
-  };
   const answer = gcd(number1, number2, 1, 2);
   return cons(question, String(answer));
 };
