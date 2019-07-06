03dc9c339c2f2815bbee195242351c6d1146458f
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-07-06 09:18:04 +0000

    update brain-calc

--- src/bin/games/brain-calc.js
+++ src/bin/games/brain-calc.js
@@ -3,28 +3,32 @@ import { cons } from '@hexlet/pairs';
 import { brainGames, getRandomIntInclusive } from '../brain-games';
 
 const task = 'What is the result of the expression?\n';
+
+const getQuestion = (n1, n2, oper) => {
+  const q = {
+    1: `${n1} + ${n2}`,
+    2: `${n1} - ${n2}`,
+    3: `${n1} * ${n2}`,
+  };
+  return q[oper];
+};
+
+const getAnswer = (n1, n2, oper) => {
+  const a = {
+    1: () => n1 + n2,
+    2: () => n1 - n2,
+    3: () => n1 * n2,
+  };
+  return a[oper]();
+};
+
 const questionAnswer = () => {
   const operation = getRandomIntInclusive(1, 3);
   const number1 = getRandomIntInclusive(1, 30);
   const number2 = getRandomIntInclusive(1, 30);
-  let question;
-  let answer;
-  switch (operation) {
-    case 1:
-      question = `${number1} + ${number2}`;
-      answer = number1 + number2;
-      break;
-
-    case 2:
-      question = `${number1} - ${number2}`;
-      answer = number1 - number2;
-      break;
-
-    default:
-      question = `${number1} * ${number2}`;
-      answer = number1 * number2;
-      break;
-  }
+  const question = getQuestion(number1, number2, operation);
+  const answer = getAnswer(number1, number2, operation);
   return cons(question, String(answer));
 };
+
 brainGames(task, questionAnswer);
