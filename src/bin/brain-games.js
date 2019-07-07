72dd46d6f8204f672bd77135fea542263d869c36
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-07-07 07:56:39 +0000

    brain-prime

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -9,28 +9,27 @@ export const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (floorMax - ceilMin + 1)) + ceilMin;
 };
 
-const isTrueAnswer = (tAnswer, uAnswer, uName, i) => {
-  if (tAnswer === uAnswer) {
+const brainGamesIter = (questAnsw, uName, i) => {
+  const pairQuestionAnswer = questAnsw();
+  const question = car(pairQuestionAnswer);
+  const trueAnswer = cdr(pairQuestionAnswer);
+  const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
+  if (trueAnswer === userAnswer) {
     console.log('Correct!');
     if (i === 2) {
       console.log(`Congratulations, ${uName}!\n`);
+      return null;
     }
-    return true;
+    return brainGamesIter(questAnsw, uName, i + 1);
   }
-  console.log(`\x1B[31m${uAnswer} \x1B[39mis wrong answer ;(. Correct answer was \x1B[31m${tAnswer}\x1B[39m.`);
+  console.log(`\x1B[31m${userAnswer} \x1B[39mis wrong answer ;(. Correct answer was \x1B[31m${trueAnswer}\x1B[39m.`);
   console.log(`Let's try again, ${uName}!\n`);
-  return false;
+  return null;
 };
 
 export const brainGames = (task, questionAnswer) => {
   console.log('\nWelcome to the Brain Games!');
   console.log(task);
   const userName = name();
-  for (let i = 0; i < 3; i += 1) {
-    const pairQuestionAnswer = questionAnswer();
-    const question = car(pairQuestionAnswer);
-    const trueAnswer = cdr(pairQuestionAnswer);
-    const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
-    if (!isTrueAnswer(trueAnswer, userAnswer, userName, i)) break;
-  }
+  return brainGamesIter(questionAnswer, userName, 0);
 };
