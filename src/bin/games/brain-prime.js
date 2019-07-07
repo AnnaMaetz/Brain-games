4af55a91f588a557092466a41f7f2feefddb7e52
Anna Maetz <sidelnikova-anne@yandex.ru>
2019-07-07 08:22:18 +0000

    refactoring brain-prime

--- src/bin/games/brain-prime.js
+++ src/bin/games/brain-prime.js
@@ -6,7 +6,7 @@ const task = 'Answer \x1B[31m"yes" \x1B[39mif given number is prime. Otherwise a
 const questionAnswer = () => {
   const primeNumbersArr = [2, 35, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71];
   const question = getRandomIntInclusive(1, 71);
-  const answer = !primeNumbersArr.indexOf(question) ? 'yes' : 'no';
+  const answer = primeNumbersArr.indexOf(question) === -1 ? 'no' : 'yes';
   return cons(question, answer);
 };
 brainGames(task, questionAnswer);
